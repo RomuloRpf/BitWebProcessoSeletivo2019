ff80acad503d44fda86a898a44c8f4e1be1141ba
geremias <djasdklsj>
2019-11-26 17:08:17 -0200

    Aula 8

--- Aulas/Aula 7/Exercicio 2/js/myScript.js
+++ Aulas/Aula 7/Exercicio 2/js/myScript.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Inicialize aqui
+  M.AutoInit();
+  var elems = document.querySelectorAll('.carousel');
+  var options = {duration:1000,dist:0,shift:0}
+  var instances = M.Carousel.init(elems,options);
   });
